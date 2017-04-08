c90a07acea701ae66a0598fa2a6b6de0c4a89bea
markodamnjanovic <marko2436@hotmail.com>
2017-04-08 10:05:35 +0200

    Commit 1

--- src/package1/Broj2.java
+++ src/package1/Broj2.java
@@ -21,7 +21,7 @@ public class Broj2 {
 
     static class BrojStatic {
         public static void staticMethod() {
-            System.out.println("1");
+            System.out.println("1111");
         }
     }
 
