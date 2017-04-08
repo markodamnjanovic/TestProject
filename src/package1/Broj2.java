30012ddef9ff037e628277e2f9af0f8bc40070fb
markodamnjanovic <marko2436@hotmail.com>
2017-04-08 21:55:49 +0200

    Removed all

--- src/package1/Broj2.java
+++ src/package1/Broj2.java
@@ -21,10 +21,6 @@ public class Broj2 {
 
     static class BrojStatic {
         public static void staticMethod() {
-            int a = 1;
-            int b = 2;
-            int c = 3;
-            int d = 4;
         }
     }
 
